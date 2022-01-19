aba9bcf5d4887d7ffc107b824b1ee00476d5efca
Josumtz7 <josu.martinezdeaguir@alumni.mondragon.edu>
2022-01-19 13:55:43 +0100

    This is the file were all the non valid channels will be detected

---
+++ nonvalidchannels.m
@@ -0,0 +1,17 @@
+%This file says the files that are wrong in each epoch folder
+% This README will detect the wrong channels in the future, task for the
+% future
+    
+    %% epoch sn1: 
+       % Missing channels: 9 and 12
+       % Giving error channels: 11
+       % Non valid channels: 4
+    %% epoch sn2
+       % Missing channels: 9, 11, 12 and 15
+       % Giving error channels: -
+       % Non valid channels: -
+    %% epoch sn3
+       % Missing channels: 9, 11, 12
+       % Giving error channels: -
+       % Non valid channels: 4
+
