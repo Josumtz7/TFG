527cc8516ec13d348b8fe9505483d2366f44901e
Josumtz7 <josu.martinezdeaguir@alumni.mondragon.edu>
2022-01-21 13:45:59 +0100

    This function will help us to obtain the data for evey folder, it will save us a lot of time in the future. Going folder by folder seeing the non valid samples is not efficient.

---
+++ folderselection.m
@@ -0,0 +1,19 @@
+    topLevelFolder = pwd; 
+    files = dir(topLevelFolder);
+    strdir = convertCharsToStrings(topLevelFolder);
+    dirFlags = [files.isdir];
+    subFolders = files(dirFlags); 
+    subFolderNames = {subFolders(3:end).name};
+    j = 1;
+    %Find the folders called epoch
+    for i = 1:1:length(subFolderNames)
+        if contains(subFolderNames(1,i),'epoch') == 1
+            epochsubfoldername(j,1) = subFolderNames(1,i);
+            j = j+1;
+        end
+    end
+    % I have done an string matrix with all the directory location of the
+    % folders we want to analyse
+    for i = 1:1:length(epochsubfoldername)
+        folderpwd(i,1) = strcat(strdir, '\',epochsubfoldername(i,1));
+    end
