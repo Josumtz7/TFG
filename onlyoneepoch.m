29b89801f4d2e273a7de46d2d0accb6ed7daff63
Josumtz7 <josu.martinezdeaguir@alumni.mondragon.edu>
2022-01-25 15:00:19 +0100

    The code has been optimized and it has been integrated also the part of the elimination of the 4th channel

--- onlyoneepoch.m
+++ onlyoneepoch.m
@@ -1,20 +1,34 @@
+%In case you want to work with only one epoch folder use this file
 clear 
+close
 pwd =  'D:\Erasmus\TFG\Neuralynx'; %Write here the place you have downloaded the epoch folders
 %Detection of files and names
 folderpwd = folderselection(pwd);
-validchann = zeros(length(folderpwd),15);
+validchann = zeros(1,15);
 %delete 'CSC11_169806023_223259735.ncs' its the only channel which is not
 %valid (from epoch sn1)
-
 DetectNcs = dir('*.ncs');
 myfolder = pwd;
 FilenameCell = {DetectNcs.name}';
-[ChannelNum, FinalFilenames] = loaddata(FilenameCell);
+
+% Delete the Channel 4 which is not correctly recorded as seeing it by
+% plotting
+
+YNCHan4 = contains(FilenameCell,'CSC4'); 
+PosChan4 = find(YNCHan4,1); 
+for i=PosChan4:length(FilenameCell)-1
+    FilenameCell{i} = FilenameCell{i+1};
+    
+end
+DetectNcs(PosChan4) = [];
+FilenameCell = FilenameCell((1:i),:);
+
 
 for k = length(FilenameCell)+1:1:15
 DetectNcs(k).name = [];
 end
 
+[ChannelNum, FinalFilenames] = loaddata(FilenameCell,0);
 %Definition of data loading characteristics with predefined values
 %(recommendation of Neuralynx)
 FieldSelectionFlags = [1 1 1 1 1];
@@ -24,6 +38,7 @@ ExtractionModeVector = 1;
     
 rst = 1;
 t = 1;
+clear ts
 for i = 1:1:length(FinalFilenames)
     j = 1;
     centi = 0;
@@ -34,14 +49,14 @@ for i = 1:1:length(FinalFilenames)
             Filename = convertStringsToChars(FinalFilenames(i,1)); 
             [Timestamps, ChannelNumbers, SampleFrequencies, NumberOfValidSamples, Samples, Header] = Nlx2MatCSC(Filename, FieldSelectionFlags, HeaderExtractionFlag, ExtractMode, ExtractionModeVector);
             epoch(:,i) = create_epoch_table(Samples, SampleFrequencies);
-            validchann(1,i) = 1;         
+            validchann(1,i) = 1;
             [downsepoch] = downsampling(epoch, SampleFrequencies); %STILL NEED DETECTION OF TIME (Previously posdown)                
         end
         j = j+1;
     end
 
-    if validchann(1,i) == 1 %to create a matrix with only valid channels
-        ts(:,t) = downsepoch(:,i);
+    if validchann(1,i) == 1  %to create a matrix with only valid channels
+        ts(:,t) = downsepoch(:,i);  
         t = t+1;
     end
 
@@ -49,7 +64,12 @@ for i = 1:1:length(FinalFilenames)
         rst = 2;
         i = 1;
     end
+   
+  NonValid(1,i) = any(NumberOfValidSamples < 512);
+  if  NonValid(1,i) == 1 %to check if there are non valid channels
+        Less512(i,:) = find(NumberOfValidSamples == 0);
+        % Only in epoch sn1 and after the 4th second, non meaningful
+  end
+  
 end
-groups = ones(t-1,1);
-Otot = hoi_exhaustive_loop_zerolag_fdr(ts,4,20,1,myfolder,groups);
-
+plot(ts,'DisplayName','ts'); xlim([0 length(downsepoch)]);
