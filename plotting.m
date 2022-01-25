b5f4367402fb24528643b6a51bdba6f879ec5b8a
Josumtz7 <josu.martinezdeaguir@alumni.mondragon.edu>
2022-01-25 14:59:23 +0100

    This new file gives the option to plot the data with each channel. This will help us to identify the wrong channels and the valid data.

---
+++ plotting.m
@@ -0,0 +1,21 @@
+% By pasting this files content in the line defined in main.m we will be
+% able to plot all the channels of all the epoch folders with the name of
+% each channel. This will be usefull as some channels are invalid.
+figure(genr)
+ NonZerosChan = nonzeros(ChannelNum);
+ [M,N] = size(ts);
+ if N < 13
+    for i=1:N
+        subplot(3,4,i);
+        plot(ts(:,i));
+        xlim([0 M]);
+        title(strcat('Channel:',num2str(NonZerosChan(i))));        
+    end
+ elseif N >= 13
+     for i=1:N
+         subplot(3,5,i)
+         plot(ts(:,i))
+         xlim([0 M]);
+         title(strcat('Channel:',num2str(NonZerosChan(i))));
+     end
+ end
