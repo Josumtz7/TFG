d83c8b46f784486cebac23b96ff1a3eb858fc693
Josumtz7 <josu.martinezdeaguir@alumni.mondragon.edu>
2022-02-21 09:13:17 +0100

    updated, still need revision

--- mean_values.m
+++ mean_values.m
@@ -1,48 +1,65 @@
-mean1 = zeros(length(downsepoch),15);
-mean2 = zeros(length(downsepoch),15);
-mean3 = zeros(length(downsepoch),15);
+function [mean1,mean2] = mean_values(downsepoch, ChannelNum, validchann)
 
-% This first mean strategy has been done with the adjoinning channels
-for i = 1:1:length(ChannelNum)
-    if i ~= 1 && i ~= length(ChannelNum) 
-        if validchann(1,i) == 0 && validchann(1,i-1) == 1 && validchann(1,i+1) == 1
-            lateral_doub = ([downsepoch(:,i-1), downsepoch(:,i+1)]);
+    % This function will be used to choose and select if necessary the best
+    % option to fill in the gaps of the missing values.
+
+    % Input:
+    %         - downsepoch: matrix with all the downsampled epoch (3596x15)
+    %         - ChannelNum: matrix with the number of the valid channels
+    %         - validchann: matrix saying if each channel is valid or not
+    % Output: 
+    %         - mean1: mean with adjoining channels
+    %         - mean2: mean taking into account the groups
+
+    mean1 = zeros(length(downsepoch),15);
+    mean2 = zeros(length(downsepoch),15);
+    
+    
+    % This first mean strategy has been done with the adjoinning channels
+    for i = 1:1:length(ChannelNum)
+        if i ~= 1 && i ~= length(ChannelNum) 
+            if validchann(1,i) == 0 && validchann(1,i-1) == 1 && validchann(1,i+1) == 1
+                lateral_doub = ([downsepoch(:,i-1), downsepoch(:,i+1)]);
+                med = mean(lateral_doub,2);
+                mean1(:,i) = med;
+            elseif validchann(1,i) == 0 && validchann(1,i-1) == 0 && validchann(1,i-2) == 1 && validchann(1,i+1) == 1
+                lateral_doub = ([downsepoch(:,i-2), downsepoch(:,i+1)]);
+                med = mean(lateral_doub,2);
+                mean1(:,i) = med;
+            elseif validchann(1,i) == 0 && validchann(1,i-1) == 1 && validchann(1,i+2) == 1 && validchann(1,i+1) == 0
+                lateral_doub = ([downsepoch(:,i-1), downsepoch(:,i+2)]);
+                med = mean(lateral_doub,2);
+                mean1(:,i) = med;
+            end
+        elseif i == length(ChannelNum)
+            lateral_doub = ([downsepoch(:,1), downsepoch(:,i-1)]); 
             med = mean(lateral_doub,2);
-            mean1(:,i) = med;
-        elseif validchann(1,i) == 0 && validchann(1,i-1) == 0 && validchann(1,i-2) == 1 && validchann(1,i+1) == 1
-            lateral_doub = ([downsepoch(:,i-2), downsepoch(:,i+1)]);
-            med = mean(lateral_doub,2);
-            mean1(:,i) = med;
-        elseif validchann(1,i) == 0 && validchann(1,i-1) == 1 && validchann(1,i+2) == 1 && validchann(1,i+1) == 0
-            lateral_doub = ([downsepoch(:,i-1), downsepoch(:,i+2)]);
-            med = mean(lateral_doub,2);
-            mean1(:,i) = med;
+            mean1(:,i) = med;  
+        end  
+        while mean1(1,i) == 0
+            mean1(:,i) = downsepoch(:,i);
         end
-    elseif i == length(ChannelNum)
-        lateral_doub = ([downsepoch(:,1), downsepoch(:,i-1)]); 
-        med = mean(lateral_doub,2);
-        mean1(:,i) = med;  
-    end  
-    while mean1(1,i) == 0
-        mean1(:,i) = downsepoch(:,i);
     end
-end
-
-% This part groups the channels from 8 to 11 as they are part of the
-% hippocampus and have relation between them
-k = 1;
-for i = 1:1:length(ChannelNum)
-    if i > 7 && i < 12 && validchann(1,i) ~= 0    
-        globmed(:,k) = downsepoch(:,i);
-        k = k + 1;       
+    
+    % This part groups the channels from 8 to 11 as they are part of the
+    % hippocampus and have relation between them. Here we should implement
+    % another technique, some values are still missing.
+    k = 1;
+    for i = 1:1:length(ChannelNum)
+        if i > 7 && i < 12 && validchann(1,i) ~= 0    
+            globmed(:,k) = downsepoch(:,i);
+            k = k + 1;       
+        end      
     end
     avalue = mean(globmed,2); 
-     if i > 7 && i < 12 && validchann(1,i) == 0    
-        mean2(:,i) = avalue;
-    end
+    for i = 1:1:length(ChannelNum)
+        if i > 7 && i < 12 && validchann(1,i) == 0    
+            mean2(:,i) = avalue;
+        elseif mean2(1,i) == 0
+            mean2(:,i) = downsepoch(:,i);
+        end
+    end    
 end
-
-
 % Plotting all the channels to se if there have been well defined
 
 % ts = mean2;
