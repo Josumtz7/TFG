84e111fc53beb5ab28c7c86a664d575f3625786d
Josumtz7 <josu.martinezdeaguir@alumni.mondragon.edu>
2022-02-21 09:13:43 +0100

    updated

--- real_channel.m
+++ real_channel.m
@@ -1,9 +1,10 @@
+function [Otot] = real_channel(maxsize, Otot, num_valid_chann)
+
 % This function will help us to detect the index values of the real
 % channels. If not with the missing data and without making use of the
 % aproximation and mean values (in short, without completing the missing
 % data) we couldn't see the real interdependencies between neural channels.
 
-function [Otot] = real_channel(maxsize, Otot, num_valid_chann)
     for i = 3:maxsize
         for j = 1:length(Otot(i).index_var_red)
             for k = 1:i
