b85394cacb9e6709cfcaa5747f0308b0fec3738a
Josumtz7 <josu.martinezdeaguir@alumni.mondragon.edu>
2022-04-22 10:04:12 +0200

    This could be a file to start and understand the basics of the code. It takes the information of only one channel recording.

---
+++ onlyfile.m
@@ -0,0 +1,13 @@
+% This could be a file to start and understand the basics of the code. It
+% takes the information of only one channel recording
+clear
+
+FieldSelectionFlags = [1 1 1 1 1];
+Filename = 'CSC1_169806023_223259735.ncs'; % to be updated for each 'CSCx.ncs' file
+HeaderExtractionFlag = 1;
+ExtractMode = 1;
+ExtractionModeVector = 1;
+myfolder = pwd;
+[Timestamps, ChannelNumbers, SampleFrequencies, NumberOfValidSamples, Samples, Header] = Nlx2MatCSC( Filename, FieldSelectionFlags, HeaderExtractionFlag, ExtractMode, ExtractionModeVector);
+epoch(:,1) = create_epoch_table(Samples, SampleFrequencies);
+downsepoch = downsampling(epoch, SampleFrequencies); 
